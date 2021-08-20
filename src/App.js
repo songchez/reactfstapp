edc081371a937abb8bf47f12f8fd492d5f95798f
songchez <tama4840@gmail.com>
2021-08-20 11:16:07 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -1,67 +1,49 @@
-import propTypes, {Proptypes} from "prop-types";
+import axios from "axios";
+import react from "react";
+import Movies from "./movies";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Movie App</h1>
-      {foodilike.map(dish =>
-        <Food
-          key ={dish.id} 
-          name ={dish.name} 
-          image ={dish.image} 
-          rating ={dish.rating}
-        />
-      )}
-    </div>
-  );
-}
+class App extends react.Component {
+  state = {
+    isloading: true,
+    movies: [],
+  };
 
- 
-const foodilike = [{
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 5.0
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
+  Getmovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json/sort_by=rating"
+    );
+    this.setState({ movies, isloading: false }); //movies:movies와 같은뜻임
+  };
+
+  componentDidMount() {
+    this.Getmovies();
   }
-];
 
-function Food({name, image, rating})   {
-  return (
-    <div className="App">
-      <h1>{name}</h1>
-      <img src = {image} alt= {name}/>
-      <h4>{rating}/5.0</h4>
-    </div>
-  );
+  render() {
+    const { isloading, movies } = this.state;
+    return (
+      <div>
+        {isloading
+          ? "Loading..."
+          : movies.map((movies) => {
+              return (
+                <Movies
+                  key={movies.id}
+                  id={movies.id}
+                  year={movies.year}
+                  title={movies.title}
+                  summary={movies.summary}
+                  poster={movies.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
+  }
 }
-
-//타입을 확인해서 잘되고있는지 디버그
-Food.prototype = {
-  name: propTypes.string.isRequiered,
-  picture: propTypes.string.isRequiered,
-  rating: propTypes.number.isRequiered
-};
 
 export default App;
